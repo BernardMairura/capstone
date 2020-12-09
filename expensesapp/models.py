57cefb48dfa41e71628ff5dca186759400d0b140
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-12-09 17:04:03 +0300

    social user

--- expensesapp/models.py
+++ expensesapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from authentication.models import User
 
 # Create your models here.
+
+
+class Expenses(models.Model):
+
+    LEDGER_LINES=[
+        ('TRAVEL','TRAVEL'),
+        ('RENT','RENT'),
+        ('INTERNET','INTERNET'),
+        ('AIRTIME','AIRTIME'),
+        ('FUEL','FUEL'),
+        ('MISCEllANEOUS','MISCEllANEOUS')
+    ]
+
+    date=models.DateField(null=False,blank=False)
+    ledger=models.CharField(choices=LEDGER_LINES,max_length=255)
+    reference=
+    description=models.TextField()
+    amount=models.DecimalField(max_digits=10,decimal_places=2,max_length=255)
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    transaction_date= models.DateTimeField(auto_now_add=True)
+    modified_date= models.DateTimeField(auto_now=True)
+
+     class Meta:
+        ordering: ['-modified_date']
+
+    def __str__(self):
+        return str(self.user)+'s income'
+
+
+
