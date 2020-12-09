2639eda86f0f46d839e0f595d90d64ec84920aeb
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-12-09 07:25:17 +0300

    custome user

--- authentication/models.py
+++ authentication/models.py
@@ -37,3 +37,28 @@ class UserManager(BaseUserManager):
 
 AUTH_PROVIDERS = { 'google': 'google','email': 'email'}
 
+
+class User(AbstractBaseUser,PermissionsMixin):
+    username=models.CharField(max_length=50,unique=True,db_index=True)
+    email=models.EmailField(max_length=150,unique=True,db_index=True)
+    is_verified=models.BooleanField(default=False)
+    is_active=models.BooleanField(default=False)
+    is_staff=models.BooleanField(default=False)
+    created_at=models.DateField(auto_now_add=True)
+    updated_at=models.DateField(auto_now_add=True)
+
+    auth_provider=models.CharField(
+        max_length=150,blank=False,
+        null=False,default=AUTH_PROVIDERS.get(email))
+
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['username']
+
+
+    objects=UserManager
+
+    def __str__(self):
+        return self.email
+
+
+
