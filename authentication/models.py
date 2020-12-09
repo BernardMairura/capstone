601f0a5dafb95a45c454d0b6404fe1b22ff66189
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-12-09 06:49:42 +0300

    add user manager to manage our model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
 
 # Create your models here.
+
+
+
+class UserManager(BaseUserManager):
+    def create_user(self,email,password=None):
+        """
+        creates and saves a User with a given mail and password.
+        """
+
+        if not mail:
+            raise ValueError('Users must have an email address')
+
+        user=self.model(
+            email=self.normalize_email(email),
+        )
+
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
